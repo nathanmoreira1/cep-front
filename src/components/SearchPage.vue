c3068a5eb7b8666c00e261fef6ce4ab81f8eba1d
Nathan Moreira de Macedo <nathanmoreirademacedo@gmail.com>
2025-01-21 15:33:35 -0300

    fix formatacao cep

--- src/components/SearchPage.vue
+++ src/components/SearchPage.vue
@@ -1,15 +1,14 @@
 <template>
-  <div
-    class="flex flex-col items-center justify-center h-screen bg-gray-100 text-center"
-  >
+  <div class="flex flex-col items-center justify-center h-screen bg-gray-100 text-center">
     <h1 class="text-3xl font-bold text-blue-600">Pesquisa de CEP</h1>
     <div class="mt-4">
       <input
         v-model="cep"
         type="text"
         placeholder="Digite o CEP"
-        maxlength="8"
+        maxlength="9"
         class="w-64 px-4 py-2 border border-gray-300 rounded shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+        @input="formatCep"
       />
       <button
         @click="searchCepHandler"
@@ -21,9 +20,7 @@
     </div>
     <!-- Loading Spinner -->
     <div v-if="loading" class="mt-4 flex items-center justify-center space-x-2">
-      <div
-        class="w-5 h-5 border-2 border-blue-500 border-t-transparent rounded-full animate-spin"
-      ></div>
+      <div class="w-5 h-5 border-2 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
       <span class="text-blue-500">Carregando...</span>
     </div>
     <!-- Endereço -->
@@ -61,9 +58,23 @@ export default {
   methods: {
     ...mapActions(["searchCep"]),
     ...mapMutations(["resetAddress"]),
+    
+    formatCep(event) {
+      let value = event.target.value.replace(/\D/g, "");
+
+      if (value.length > 5) {
+        value = value.replace(/^(\d{5})(\d{1,3})$/, "$1-$2");
+      }
+
+      this.cep = value;
+    },
+
     async searchCepHandler() {
       this.loading = true;
-      await this.searchCep(this.cep);
+
+      const cepWithoutHyphen = this.cep.replace("-", "");
+
+      await this.searchCep(cepWithoutHyphen);
       this.loading = false;
     },
   },
